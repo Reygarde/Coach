02409981fb30963d28af44db3cf8a71c8d78e714
kevin <kevin.schoubert@accenture.com>
2024-08-04 23:40:43 +0200

    V1

---
+++ middleware.ts
@@ -0,0 +1,34 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+
+  const isPublicRoute = createRouteMatcher( [
+    "/sign-in(.*)",
+    "/sign-out(.*)<",
+    "/",
+    "/profile",
+    "/api/assistant/create",
+    "/api/thread",
+    "/api/message/create",
+    "/api/message/list",
+    "/api/run/create",
+    "/api/run/retrieve",
+    "/api/challenge-users",
+    "/api/openai",
+    "/api/send-notifications",
+  ],
+);
+
+export default clerkMiddleware((auth, request) => {
+  if(!isPublicRoute(request)) {
+    auth().protect();
+  }
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
